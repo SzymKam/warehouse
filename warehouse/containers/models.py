94688084e032ee62d3c7b9f044d6574dd12fce08
SzymKam <szymon15kaminski@gmail.com>
2023-03-19 16:20:52 +0100

    feat: add new special models of medical equipment

--- warehouse/containers/models.py
+++ warehouse/containers/models.py
@@ -6,6 +6,19 @@ MEDICAL_EQUIPMENT_EQUIPMENT_TYPE_CHOICES = []
 
 CONTAINER_NAME_CHOICES = []
 
+DRUG_ACTIVE_SUBSTANCES = []
+DRUG_DOSAGE_FORM = []
+
+FLUID_VOLUME = []
+
+CANNULA_SIZE = []
+
+NEEDLE_VOLUME = []
+
+SYRINGE_SIZE = []
+
+BIG_SIZE = []
+
 
 class Container(models.Model):
     name = models.CharField(max_length=50, choices=CONTAINER_NAME_CHOICES)
@@ -27,6 +40,35 @@ class MedicalEquipment(models.Model):
     amount = models.IntegerField(null=True, blank=True)
     expiration_date = models.DateField(null=True, blank=True)
     equipment_type = models.CharField(
-        max_length=50, choices=MEDICAL_EQUIPMENT_EQUIPMENT_TYPE_CHOICES
+        null=True,
+        blank=True,
+        max_length=50,
+        choices=MEDICAL_EQUIPMENT_EQUIPMENT_TYPE_CHOICES,
     )
     description = models.TextField(null=True, blank=True)
+
+
+class Drug(MedicalEquipment):
+    active_substance = models.CharField(choices=DRUG_ACTIVE_SUBSTANCES)
+    psychotropic_or_narcotic = models.BooleanField()
+    dosage_form = models.CharField(choices=DRUG_DOSAGE_FORM, blank=True, null=True)
+
+
+class Fluid(MedicalEquipment):
+    volume = models.CharField(choices=FLUID_VOLUME)
+
+
+class Cannula(MedicalEquipment):
+    size = models.CharField(choices=CANNULA_SIZE)
+
+
+class Needle(MedicalEquipment):
+    volume = models.CharField(choices=NEEDLE_VOLUME)
+
+
+class Syringe(MedicalEquipment):
+    size = models.CharField(choices=SYRINGE_SIZE)
+
+
+class BIG:
+    size = models.CharField(choices=BIG_SIZE)
