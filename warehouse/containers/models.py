38866d801af74e8b3d56a2258d85817ec1692113
SzymKam <szymon15kaminski@gmail.com>
2023-03-20 10:43:51 +0100

    feat: add rest of new special models of medical equipment

--- warehouse/containers/models.py
+++ warehouse/containers/models.py
@@ -19,6 +19,27 @@ SYRINGE_SIZE = []
 
 BIG_SIZE = []
 
+LT_TUBE_SIZE = []
+
+GLOVES_SIZE = []
+
+STERILE_GLOVES_SIZE = []
+
+GAUZE_SIZE = []
+
+NPA_TUBE_SIZE = []
+
+OPA_TUBE_SIZE = []
+
+ET_TUBE_SIZE = []
+
+BLADE_SIZE = []
+
+O2_MASK_TYPE = []
+O2_MASK_SIZE = []
+
+VENTILATION_MASK_SIZE = []
+
 
 class Container(models.Model):
     name = models.CharField(max_length=50, choices=CONTAINER_NAME_CHOICES)
@@ -48,6 +69,9 @@ class MedicalEquipment(models.Model):
     description = models.TextField(null=True, blank=True)
 
 
+"""classes below, are classes for special medical equipment, requires special field/s"""
+
+
 class Drug(MedicalEquipment):
     active_substance = models.CharField(choices=DRUG_ACTIVE_SUBSTANCES)
     psychotropic_or_narcotic = models.BooleanField()
@@ -70,5 +94,46 @@ class Syringe(MedicalEquipment):
     size = models.CharField(choices=SYRINGE_SIZE)
 
 
-class BIG:
+class BIG(MedicalEquipment):
     size = models.CharField(choices=BIG_SIZE)
+
+
+class LTTube(MedicalEquipment):
+    size = models.CharField(choices=LT_TUBE_SIZE)
+
+
+class Gloves(MedicalEquipment):
+    size = models.CharField(choices=GLOVES_SIZE)
+
+
+class SterileGloves(MedicalEquipment):
+    size = models.CharField(choices=STERILE_GLOVES_SIZE)
+
+
+class Gauze(MedicalEquipment):
+    size = models.CharField(choices=GAUZE_SIZE)
+
+
+class NasopharyngealTube(MedicalEquipment):
+    size = models.CharField(choices=NPA_TUBE_SIZE)
+
+
+class OropharyngealTube(MedicalEquipment):
+    size = models.CharField(choices=OPA_TUBE_SIZE)
+
+
+class EndotrachealTube(MedicalEquipment):
+    size = models.CharField(choices=ET_TUBE_SIZE)
+
+
+class LaryngoscopeBlade(MedicalEquipment):
+    size = models.CharField(choices=BLADE_SIZE)
+
+
+class OxygenMask(MedicalEquipment):
+    type = models.CharField(choices=O2_MASK_TYPE)
+    size = models.CharField(choices=O2_MASK_SIZE)
+
+
+class VentilationMask(MedicalEquipment):
+    size = models.CharField(choices=VENTILATION_MASK_SIZE)
