8a0c90ab57400fce5613ddfbcc37bf55b0572196
SzymKam <szymon15kaminski@gmail.com>
2023-03-21 00:35:34 +0100

    feat: add models to admin panel

--- warehouse/containers/admin.py
+++ warehouse/containers/admin.py
@@ -1,3 +1,49 @@
 from django.contrib import admin
+from .models import (
+    MedicalEquipment,
+    Container,
+    Drug,
+    Cannula,
+    EndotrachealTube,
+    Fluid,
+    Gauze,
+    Gloves,
+    VentilationMask,
+    OxygenMask,
+    OropharyngealTube,
+    NasopharyngealTube,
+    LaryngoscopeBlade,
+    BIG,
+    LTTube,
+    Needle,
+    Syringe,
+    SterileGloves,
+)
 
-# Register your models here.
+
+@admin.register(
+    MedicalEquipment,
+    Drug,
+    Cannula,
+    EndotrachealTube,
+    Fluid,
+    Gauze,
+    Gloves,
+    VentilationMask,
+    OxygenMask,
+    OropharyngealTube,
+    NasopharyngealTube,
+    LaryngoscopeBlade,
+    BIG,
+    LTTube,
+    Needle,
+    Syringe,
+    SterileGloves,
+)
+class EquipmentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Container)
+class ContainerAdmin(admin.ModelAdmin):
+    pass
