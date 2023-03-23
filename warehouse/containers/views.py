6082137ebc3c3e8e1f116c720c10a12c5f48ec6f
SzymKam <szymon15kaminski@gmail.com>
2023-03-23 16:08:34 +0100

    feat: add vievs and urls for all containers and detail

--- warehouse/containers/views.py
+++ warehouse/containers/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Container
 
-# Create your views here.
+
+# class ContainerListView(ListView):
+#     model = Container
+#     template_name = "containers/containers-home.html"
+#
+
+
+class ContainerDetailView(DetailView):
+    model = Container
+    template_name = "containers/containers-detail.html"
+
+
+def container_list(request):
+    containers = Container.objects.all()
+    return render(
+        request,
+        "containers/containers-home.html",
+        {"title": "Containers", "containers": containers},
+    )
+
+
+def container_detail(request, id):
+    container = Container.objects.filter(id=id)
+    print(container)
+    return render(
+        request,
+        "containers/containers-detail.html",
+        {"title": "Container", "container": container},
+    )
