aa1d15816f09ba59b5655fa838ee5c57c49f5933
SzymKam <szymon15kaminski@gmail.com>
2023-03-25 00:24:20 +0100

    feat: update files

--- warehouse/containers/urls.py
+++ warehouse/containers/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 
 # from .views import ContainerDetailView, ContainerListView, ContainerDetailView
-from .views import container_list, container_detail, warehouse_main
+from .views import container_list, container_detail, warehouse_main, container_create
 
 
 urlpatterns = [
@@ -10,4 +10,5 @@ urlpatterns = [
     path("", warehouse_main, name="warehouse-main"),
     path("containers/", container_list, name="containers-home"),
     path("containers/<int:id>", container_detail, name="containers-detail"),
+    path("containers/create", container_create, name="containers-create"),
 ]
