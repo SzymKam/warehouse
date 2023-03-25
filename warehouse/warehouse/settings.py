7054f524d8b36df315049e52c00ab9e3dfda409d
SzymKam <szymon15kaminski@gmail.com>
2023-03-25 16:51:55 +0100

    feat: update settings file for bootstrap forms

--- warehouse/warehouse/settings.py
+++ warehouse/warehouse/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     "django_extensions",
 ]
 
-INSTALLED_EXTENSIONS = ["containers"]
+INSTALLED_EXTENSIONS = ["containers", "crispy_forms", "crispy_bootstrap4"]
 
 INSTALLED_APPS += INSTALLED_EXTENSIONS
 
@@ -127,3 +127,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
