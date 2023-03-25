5dd646153cbc4172f3fdcff788de7390b81b7c95
SzymKam <szymon15kaminski@gmail.com>
2023-03-25 16:52:52 +0100

    feat: update urls file for delete and update ednpoints

--- warehouse/containers/urls.py
+++ warehouse/containers/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path, include
-
-# from .views import ContainerDetailView, ContainerListView, ContainerDetailView
-from .views import container_list, container_detail, warehouse_main, container_create
+from .views import DeleteView
+from .views import (
+    container_list,
+    container_detail,
+    warehouse_main,
+    container_create,
+    container_update,
+    container_delete,
+)
 
 
 urlpatterns = [
@@ -11,4 +17,6 @@ urlpatterns = [
     path("containers/", container_list, name="containers-home"),
     path("containers/<int:id>", container_detail, name="containers-detail"),
     path("containers/create", container_create, name="containers-create"),
+    path("containers/delete/<int:id>", container_delete, name="containers-delete"),
+    path("containers/update/<int:id>", container_update, name="containers-update"),
 ]
