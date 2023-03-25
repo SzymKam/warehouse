49302563a7ba3b35c44f222ca4377e5335a0a506
SzymKam <szymon15kaminski@gmail.com>
2023-03-25 16:54:14 +0100

    feat: update views for update and delete Containers

--- warehouse/containers/views.py
+++ warehouse/containers/views.py
@@ -1,18 +1,17 @@
-from django.shortcuts import render, redirect
-from django.views.generic import ListView, DetailView
+from typing import Type
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import (
+    ListView,
+    DetailView,
+    DeleteView,
+    UpdateView,
+    CreateView,
+)
 from .models import Container, MedicalEquipment
 from .forms import ContainerForm
+from django.contrib import messages
 
 
-# class ContainerListView(ListView):
-#     model = Container
-#     template_name = "containers/containers-home.html"
-#
-
-
-# class ContainerDetailView(DetailView):
-#     model = Container
-#     template_name = "containers/containers-detail.html"
 def warehouse_main(request):
     """render main warehouse page"""
     return render(
@@ -32,9 +31,9 @@ def container_list(request):
 
 def container_detail(request, id):
     """render detail about container by its id"""
-    container = Container.objects.filter(id=id)
+    container = get_object_or_404(Container, id=id)
     equipment = MedicalEquipment.objects.filter(container=id)
-    name = container[0].name
+    name = container.name
     return render(
         request,
         "containers/containers-detail.html",
@@ -49,5 +48,33 @@ def container_create(request):
         form = ContainerForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, "Created container!")
             return redirect("containers-home")
     return render(request, "containers/containers-create.html", {"form": form})
+
+
+def container_delete(request, id):
+    """delete container"""
+    container = get_object_or_404(Container, id=id)
+    if request.method == "POST":
+        container.delete()
+        return redirect("containers-home")
+    return render(
+        request, "containers/containers-delete.html", {"container": container}
+    )
+
+
+def container_update(request, id):
+    """update container"""
+    container = get_object_or_404(Container, id=id)
+    form = ContainerForm(request.POST or None, instance=container)
+    if form.is_valid():
+        form.save()
+        return redirect("containers-detail", id=id)
+    return render(request, "containers/containers-update.html", {"form": form})
+
+
+# class ContainerDelete(DeleteView):
+#     template_name = 'containers/containers-delete.html'
+#     success_url = "containers-home"
+#     model = Container
