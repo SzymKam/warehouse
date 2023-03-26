d9d955aedb7b5ac566c2f6ea587acda17094050b
SzymKam <szymon15kaminski@gmail.com>
2023-03-26 10:16:49 +0200

    feat: change views and urls for operating on class based views

--- warehouse/containers/urls.py
+++ warehouse/containers/urls.py
@@ -1,22 +1,27 @@
-from django.urls import path, include
-from .views import DeleteView
+from django.urls import path
 from .views import (
-    container_list,
-    container_detail,
-    warehouse_main,
-    container_create,
-    container_update,
-    container_delete,
+    ContainerDelete,
+    ContainerUpdate,
+    ContainerCreate,
+    ContainerView,
+    ContainerDetail,
 )
+from .views import warehouse_main
 
 
 urlpatterns = [
-    # path('', ContainerListView.as_view(), name='containers-home'),
-    # path('<int:pk>/', ContainerDetailView.as_view(), name='containers-detail'),
     path("", warehouse_main, name="warehouse-main"),
-    path("containers/", container_list, name="containers-home"),
-    path("containers/<int:id>", container_detail, name="containers-detail"),
-    path("containers/create", container_create, name="containers-create"),
-    path("containers/delete/<int:id>", container_delete, name="containers-delete"),
-    path("containers/update/<int:id>", container_update, name="containers-update"),
+    path("containers/", ContainerView.as_view(), name="containers-home"),
+    path("containers/<int:pk>", ContainerDetail.as_view(), name="containers-detail"),
+    path(
+        "containers/update/<int:pk>",
+        ContainerUpdate.as_view(),
+        name="containers-update",
+    ),
+    path("containers/create", ContainerCreate.as_view(), name="containers-create"),
+    path(
+        "containers/delete/<int:pk>",
+        ContainerDelete.as_view(),
+        name="containers-delete",
+    ),
 ]
--- warehouse/containers/views.py
+++ warehouse/containers/views.py
@@ -1,4 +1,3 @@
-from typing import Type
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import (
     ListView,
@@ -10,6 +9,7 @@ from django.views.generic import (
 from .models import Container, MedicalEquipment
 from .forms import ContainerForm
 from django.contrib import messages
+from django.urls import reverse_lazy
 
 
 def warehouse_main(request):
@@ -19,62 +19,56 @@ def warehouse_main(request):
     )
 
 
-def container_list(request):
-    """render list of containers"""
-    containers = Container.objects.all()
-    return render(
-        request,
-        "containers/containers-home.html",
-        {"title": "Containers", "containers": containers},
-    )
+class ContainerView(ListView):
+    queryset = Container.objects.all()
+    template_name = "containers/containers-home.html"
+
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
 
 
-def container_detail(request, id):
-    """render detail about container by its id"""
-    container = get_object_or_404(Container, id=id)
-    equipment = MedicalEquipment.objects.filter(container=id)
-    name = container.name
-    return render(
-        request,
-        "containers/containers-detail.html",
-        {"title": name, "container": container, "equipment": equipment},
-    )
+class ContainerDetail(DetailView):
+    template_name = "containers/containers-detail.html"
+    queryset = Container.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["equipment"] = MedicalEquipment.objects.filter(container=self.object.id)
+        return context
 
 
-def container_create(request):
-    """create new container"""
-    form = ContainerForm()
-    if request.method == "POST":
-        form = ContainerForm(request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(request, "Created container!")
-            return redirect("containers-home")
-    return render(request, "containers/containers-create.html", {"form": form})
+class ContainerUpdate(UpdateView):
+    model = Container
+    template_name = "containers/containers-update.html"
+    form_class = ContainerForm
+
+    def get_success_url(self):
+        return reverse_lazy("containers-detail", args=(self.object.id,))
+
+    def form_valid(self, form):
+        messages.info(self.request, "Container updated!")
+        return super().form_valid(form)
 
 
-def container_delete(request, id):
-    """delete container"""
-    container = get_object_or_404(Container, id=id)
-    if request.method == "POST":
-        container.delete()
-        return redirect("containers-home")
-    return render(
-        request, "containers/containers-delete.html", {"container": container}
-    )
+class BaseContainer:
+    model = Container
+    success_url = reverse_lazy("containers-home")
 
 
-def container_update(request, id):
-    """update container"""
-    container = get_object_or_404(Container, id=id)
-    form = ContainerForm(request.POST or None, instance=container)
-    if form.is_valid():
-        form.save()
-        return redirect("containers-detail", id=id)
-    return render(request, "containers/containers-update.html", {"form": form})
+class ContainerCreate(BaseContainer, CreateView):
+    template_name = "containers/containers-create.html"
+    form_class = ContainerForm
+
+    queryset = Container.objects.all()
+
+    def form_valid(self, form):
+        messages.success(self.request, "Container crated!")
+        return super().form_valid(form)
 
 
-# class ContainerDelete(DeleteView):
-#     template_name = 'containers/containers-delete.html'
-#     success_url = "containers-home"
-#     model = Container
+class ContainerDelete(BaseContainer, DeleteView):
+    template_name = "containers/containers-delete.html"
+
+    def form_valid(self, form):
+        messages.warning(self.request, "Container deleted!")
+        return super().form_valid(form)
