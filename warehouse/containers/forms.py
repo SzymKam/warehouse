9687863e1d2fc3206f698712ba5127615eae664e
SzymKam <szymon15kaminski@gmail.com>
2023-03-26 10:13:04 +0200

    feat: add exculed choice to model

--- warehouse/containers/forms.py
+++ warehouse/containers/forms.py
@@ -3,6 +3,14 @@ from .models import Container
 
 
 class ContainerForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        excluded_choice = "Main warehouse"
+        if excluded_choice in dict(self.fields["name"].choices):
+            choices = list(self.fields["name"].choices)
+            choices.remove((excluded_choice, excluded_choice))
+            self.fields["name"].choices = choices
+
     class Meta:
         model = Container
         fields = "__all__"
