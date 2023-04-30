7023e7d986ed2461d53e707e98445216de9f060b
SzymKam <szymon15kaminski@gmail.com>
2023-04-30 18:23:38 +0200

    feat: add and update views for updading, delete and register Staff. Add urls for these functions

--- warehouse/staff/urls.py
+++ warehouse/staff/urls.py
@@ -1,10 +1,20 @@
 from django.urls import path
-from .views import all_staff, register, StaffLogin, StaffLogout, update
+from .views import (
+    all_staff,
+    register,
+    StaffLogin,
+    StaffLogout,
+    update_by_admin,
+    update_by_user,
+    delete_user,
+)
 
 urlpatterns = [
     path("login/", StaffLogin.as_view(), name="login"),
     path("logout/", StaffLogout.as_view(), name="logout"),
     path("register/", register, name="register"),
-    path("update/<int:pk>", update, name="update"),
+    path("update/<int:pk>", update_by_user, name="update"),
+    path("update/admin/<int:pk>", update_by_admin, name="admin-update"),
+    path("delete/<int:pk>", delete_user, name="delete-user"),
     path("", all_staff, name="all-staff"),
 ]
