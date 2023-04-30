9711f1f646a18c2305f287e9fde018a2d2eb21e3
SzymKam <szymon15kaminski@gmail.com>
2023-04-30 18:23:22 +0200

    feat: add and update views for updading, delete and register Staff. Add urls for these functions

--- warehouse/staff/admin.py
+++ warehouse/staff/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Staff
+from .models import StaffModel
 
 
-@admin.register(Staff)
+@admin.register(StaffModel)
 class StaffAdmin(admin.ModelAdmin):
     pass
