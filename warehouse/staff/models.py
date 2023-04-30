71b02b78ff7e01a7b1ca5d353bcd13d132b3b602
SzymKam <szymon15kaminski@gmail.com>
2023-04-30 18:12:44 +0200

    fix: change inheritance Staff from User to AbstractUser model

--- warehouse/staff/models.py
+++ warehouse/staff/models.py
@@ -1,13 +1,12 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 from .constants import QUALIFICATIONS
 
 
-class Staff(User):
+class StaffModel(AbstractUser):
     position = models.CharField(
         max_length=50, blank=True, null=True, help_text="role in GRM"
     )
-    image = models.ImageField(default="grm_logo.jpg")
     medical_qualifications = models.CharField(
         max_length=50,
         choices=QUALIFICATIONS,
@@ -15,6 +14,7 @@ class Staff(User):
         help_text="Qualifications",
     )
     qualifications_expiration_date = models.DateField(blank=True, null=True)
+    image = models.ImageField(default="grm_logo.jpg", upload_to="profile_pictures")
 
     def __str__(self):
-        return f"{self.username} - {self.medical_qualifications}"
+        return f"{self.username} - {self.medical_qualifications}, {self.position}"
