76c5d98d7632267558944b83f9bdf49249576eea
SzymKam <szymon15kaminski@gmail.com>
2023-05-09 00:58:30 +0200

    fix: update env file

--- warehouse/warehouse/env.py
+++ warehouse/warehouse/env.py
@@ -4,4 +4,6 @@ from django.core.management.utils import get_random_secret_key
 env = environ.Env(
     DEBUG=(bool, False),
     SECRET_KEY=(str, get_random_secret_key()),
+    USER=(str, "user"),
+    PASSWORD=(str, "user"),
 )
