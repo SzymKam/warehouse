a5b51540b56f57585d2684f4d05455badb94f992
SzymKam <szymon15kaminski@gmail.com>
2023-05-09 00:59:40 +0200

    feat: add handler for 403 error

--- warehouse/warehouse/urls.py
+++ warehouse/warehouse/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import handler403
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("containers.urls")),
     path("staff/", include("staff.urls")),
 ]
+
+handler403 = "containers.views.error_403"
