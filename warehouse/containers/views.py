3d66a76fe9c33518897a9f3a7e975fde79b67469
SzymKam <szymon15kaminski@gmail.com>
2023-05-21 18:08:23 +0200

    feat: update save to pdf function

--- warehouse/containers/views.py
+++ warehouse/containers/views.py
@@ -308,29 +308,48 @@ class EquipmentDelete:
 
 
 from django.shortcuts import render, HttpResponse
-from django.template.loader import get_template, render_to_string
-from xhtml2pdf import pisa
+from django.template.loader import get_template
+from weasyprint import HTML
+from staff.models import StaffModel
 
 
-def save_to_pdf(request):
-    # map_items = {
-    #     'staff': Staff,
-    #     'containers': Container,
-    #
-    # }
-    # template_path = "containers/containers-list-for-render.html"
-    # template = get_template(template_path)
-    context = {"object_list": Container.objects.all()}
-    # html = template.render(context)
-    html = render_to_string(
-        "containers/containers-list-for-render.html", context=context
-    )
-    # html = f"{template_path, context}"
+def save_to_pdf(request, element, element_id=None):
+    map_elements = {
+        "containers": {
+            "object_list": Container.objects.all(),
+            "template": "containers/containers-list-pdf.html",
+        },
+        "all": {
+            "object_list": [model.objects.all() for model in MODEL_LIST],
+            "template": "containers/equipment-all-pdf.html",
+        },
+        "staff": {
+            "object_list": StaffModel.objects.all(),
+            "template": "staff/staff-all-pdf.html",
+        },
+    }
+    if type(element_id) is int:
+        container = {
+            "container": {
+                "object_list": {
+                    "container": Container.objects.filter(pk=element_id),
+                    "equipment": [
+                        model.objects.filter(container=element_id)
+                        for model in MODEL_LIST
+                    ],
+                },
+                "template": "containers/containers-detail-pdf.html",
+            }
+        }
+        map_elements.update(container)
 
-    response = HttpResponse(content_type="")
-    response["Content-Disposition"] = 'attachment; filename="my_pdf.pdf"'
-    pdf = pisa.CreatePDF(html, dest=response)
-    print(response)
-    if pdf.err:
-        return HttpResponse("Error while creating PDF: %s" % pdf.err)
+    template = get_template(map_elements[element]["template"])
+    context = {"object_list": map_elements[element]["object_list"]}
+    html = template.render(context)
+
+    pdf_file = HTML(string=html).write_pdf()
+    response = HttpResponse(content_type="text/pdf")
+    response["Content-Disposition"] = f'attachment; filename="{element}.pdf"'
+    response.write(pdf_file)
+
     return response
