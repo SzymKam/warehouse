042d64ed44e44dcfc4043cd4df3a58c152537de8
SzymKam <szymon15kaminski@gmail.com>
2023-06-10 23:03:10 +0200

    fix: remove login_required to views; change url to handler 403 error

--- warehouse/staff/urls.py
+++ warehouse/staff/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from django.contrib.auth.decorators import login_required
-from .views import (
+from staff.views.reset_password import (
+    MyPasswordResetView,
+    MyPasswordResetDoneView,
+    MyPasswordResetCompleteView,
+    MyPasswordResetConfirmView,
+)
+from staff.views.staff_management import (
     StaffRegister,
     StaffLogin,
     StaffLogout,
@@ -11,19 +16,28 @@ from .views import (
 from django.conf.urls.static import static
 from django.conf import settings
 
-
 urlpatterns = [
     path("login/", StaffLogin.as_view(), name="login"),
     path("logout/", StaffLogout.as_view(), name="logout"),
-    path("register/", login_required(StaffRegister.register), name="register"),
-    path("update/<int:pk>", login_required(StaffUpdate.update_by_user), name="update"),
+    path("register/", StaffRegister.register, name="register"),
+    path("update/<int:pk>", StaffUpdate.update_by_user, name="update"),
+    path("update/admin/<int:pk>", StaffUpdate.update_by_admin, name="admin-update"),
+    path("delete/<int:pk>", StaffDelete.delete_user, name="delete-user"),
+    path("", AllStaff.as_view(), name="all-staff"),
+    path("reset_password/", MyPasswordResetView.as_view(), name="reset-password"),
     path(
-        "update/admin/<int:pk>",
-        login_required(StaffUpdate.update_by_admin),
-        name="admin-update",
+        "reset_password_sent/",
+        MyPasswordResetDoneView.as_view(),
+        name="password_reset_done",
     ),
     path(
-        "delete/<int:pk>", login_required(StaffDelete.delete_user), name="delete-user"
+        "reset/<uidb64>/<token>",
+        MyPasswordResetConfirmView.as_view(),
+        name="password_reset_confirm",
     ),
-    path("", login_required(AllStaff.as_view()), name="all-staff"),
+    path(
+        "reset_password_complete/",
+        MyPasswordResetCompleteView.as_view(),
+        name="password_reset_complete",
+    ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
