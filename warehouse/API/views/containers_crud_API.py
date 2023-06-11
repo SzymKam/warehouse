829868ef22bb9a04010c9b43a97ef52fc3ddfcb4
SzymKam <szymon15kaminski@gmail.com>
2023-06-11 20:53:17 +0200

    feat: add IsAuthenticated permission into classes in views

--- warehouse/API/views/containers_crud_API.py
+++ warehouse/API/views/containers_crud_API.py
@@ -5,29 +5,35 @@ from rest_framework.generics import (
     DestroyAPIView,
     UpdateAPIView,
 )
-from API.serializers import ContainerSerializer
+from API.serializers.containers_serializer import ContainerSerializer
 from containers.models import Container
+from rest_framework.permissions import IsAuthenticated
 
 
 class GetAllContainers(ListAPIView):
+    permission_classes = [IsAuthenticated]
     serializer_class = ContainerSerializer
     queryset = Container.objects.all()
 
 
 class GetContainer(RetrieveAPIView):
+    permission_classes = [IsAuthenticated]
     serializer_class = ContainerSerializer
     queryset = Container
 
 
 class CreateContainer(CreateAPIView):
+    permission_classes = [IsAuthenticated]
     serializer_class = ContainerSerializer
 
 
 class UpdateContainer(UpdateAPIView):
+    permission_classes = [IsAuthenticated]
     serializer_class = ContainerSerializer
     queryset = Container
 
 
 class DeleteContainer(DestroyAPIView):
+    permission_classes = [IsAuthenticated]
     serializer_class = ContainerSerializer
     queryset = Container
--- warehouse/API/views/staff_crud_API.py
+++ warehouse/API/views/staff_crud_API.py
@@ -5,29 +5,35 @@ from rest_framework.generics import (
     DestroyAPIView,
     UpdateAPIView,
 )
-from API.serializers import StaffSerializer
+from API.serializers.staff_serialzer import StaffSerializer
 from staff.models import StaffModel
+from rest_framework.permissions import IsAuthenticated
 
 
 class GetAllStaff(ListAPIView):
+    permission_classes = [IsAuthenticated]
     serializer_class = StaffSerializer
     queryset = StaffModel.objects.all()
 
 
 class GetStaff(RetrieveAPIView):
+    permission_classes = [IsAuthenticated]
     serializer_class = StaffSerializer
     queryset = StaffModel
 
 
 class CreateStaff(CreateAPIView):
+    permission_classes = [IsAuthenticated]
     serializer_class = StaffSerializer
 
 
 class UpdateStaff(UpdateAPIView):
+    permission_classes = [IsAuthenticated]
     serializer_class = StaffSerializer
     queryset = StaffModel
 
 
 class DeleteStaff(DestroyAPIView):
+    permission_classes = [IsAuthenticated]
     serializer_class = StaffSerializer
     queryset = StaffModel
