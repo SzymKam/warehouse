5785eb4ca0a27f247e0d97446cc2eaef0244e0bd
SzymKam <szymon15kaminski@gmail.com>
2023-06-12 00:06:56 +0200

    feat: update serializers - add combined equipment serializer, and split container serializer

--- warehouse/API/serializers/containers_serializer.py
+++ warehouse/API/serializers/containers_serializer.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
 from containers.models import Container
+from .equipment_serializer import AllEquipmentSerializer
 
 
-class ContainerSerializer(serializers.ModelSerializer):
+class AllContainerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Container
+        fields = "__all__"
+
+
+class SingleContainerSerializer(serializers.ModelSerializer, AllEquipmentSerializer):
     class Meta:
         model = Container
         fields = "__all__"
--- warehouse/API/serializers/equipment_serializer.py
+++ warehouse/API/serializers/equipment_serializer.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.serializers import Serializer
 from containers.models import (
     MedicalEquipment,
     Drug,
@@ -20,7 +21,128 @@ from containers.models import (
 )
 
 
-class EquipmentSerializer(serializers.ModelSerializer):
+class MedicalEquipmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = MedicalEquipment
         fields = "__all__"
+
+
+class DrugSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Drug
+        fields = "__all__"
+
+
+class FluidSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Fluid
+        fields = "__all__"
+
+
+class CannulaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cannula
+        fields = "__all__"
+
+
+class NeedleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Needle
+        fields = "__all__"
+
+
+class SyringeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Syringe
+        fields = "__all__"
+
+
+class BIGSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BIG
+        fields = "__all__"
+
+
+class LtTubeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = LtTube
+        fields = "__all__"
+
+
+class GlovesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Gloves
+        fields = "__all__"
+
+
+class SterileGlovesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SterileGloves
+        fields = "__all__"
+
+
+class GauzeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Gauze
+        fields = "__all__"
+
+
+class NasopharyngealTubeGlovesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = NasopharyngealTube
+        fields = "__all__"
+
+
+class OropharyngealTubeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OropharyngealTube
+        fields = "__all__"
+
+
+class EndotrachealTubeGlovesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = EndotrachealTube
+        fields = "__all__"
+
+
+class LaryngoscopeBladeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = LaryngoscopeBlade
+        fields = "__all__"
+
+
+class OxygenMaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OxygenMask
+        fields = "__all__"
+
+
+class VentilationMaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = VentilationMask
+        fields = "__all__"
+
+
+class AllEquipmentSerializer(Serializer):
+    """
+    Serializer with all equipments items combined.
+
+    """
+
+    medical_equipment = MedicalEquipmentSerializer(many=True, read_only=True)
+    drug = DrugSerializer(many=True, read_only=True)
+    fluid = FluidSerializer(many=True, read_only=True)
+    cannula = CannulaSerializer(many=True, read_only=True)
+    needle = NeedleSerializer(many=True, read_only=True)
+    syringe = SyringeSerializer(many=True, read_only=True)
+    big = BIGSerializer(many=True, read_only=True)
+    lt_tube = LtTubeSerializer(many=True, read_only=True)
+    gloves = GlovesSerializer(many=True, read_only=True)
+    sterile_gloves = SterileGlovesSerializer(many=True, read_only=True)
+    gauze = GauzeSerializer(many=True, read_only=True)
+    npa_tube = NasopharyngealTubeGlovesSerializer(many=True, read_only=True)
+    opa_tube = OropharyngealTubeSerializer(many=True, read_only=True)
+    et_tube = EndotrachealTubeGlovesSerializer(many=True, read_only=True)
+    laryngoscope_blade = LaryngoscopeBladeSerializer(many=True, read_only=True)
+    oxygen_mask = OxygenMaskSerializer(many=True, read_only=True)
+    ventilation_mask = VentilationMaskSerializer(many=True, read_only=True)
