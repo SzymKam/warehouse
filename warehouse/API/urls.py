4017421aea636297bbd3e894705e8ecbb5a1ba4d
SzymKam <szymon15kaminski@gmail.com>
2023-06-11 18:29:02 +0200

    feat: add urls for new containers and staff api views

--- warehouse/API/urls.py
+++ warehouse/API/urls.py
@@ -1,11 +1,39 @@
 from django.urls import path
-from .views import GetAllContainers, GetContainer, CreateContainer, DeleteContainer
+from API.views.containers_crud_API import (
+    GetAllContainers,
+    GetContainer,
+    CreateContainer,
+    DeleteContainer,
+    UpdateContainer,
+)
+from API.views.staff_crud_API import (
+    GetAllStaff,
+    GetStaff,
+    CreateStaff,
+    DeleteStaff,
+    UpdateStaff,
+)
 
 urlpatterns = [
+    # containers crud
     path("containers/", GetAllContainers.as_view(), name="get-all-containers"),
     path("containers/<int:pk>/", GetContainer.as_view(), name="get-container"),
     path("containers/create/", CreateContainer.as_view(), name="create-container"),
     path(
-        "containers/delete/<int:pk>", DeleteContainer.as_view(), name="delete-container"
+        "containers/delete/<int:pk>/",
+        DeleteContainer.as_view(),
+        name="delete-container",
     ),
+    path(
+        "containers/update/<int:pk>/",
+        UpdateContainer.as_view(),
+        name="update-container",
+    ),
+    # equipment crud
+    # staff crud
+    path("staff/", GetAllStaff.as_view(), name="get-all-staff"),
+    path("staff/<int:pk>/", GetStaff.as_view(), name="get-staff"),
+    path("staff/create/", CreateStaff.as_view(), name="create-staff"),
+    path("staff/delete/<int:pk>/", DeleteStaff.as_view(), name="delete-staff"),
+    path("staff/update/<int:pk>/", UpdateStaff.as_view(), name="update-staff"),
 ]
