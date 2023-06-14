f7eadbd0c15c9ca36b8cb84323d1b28f3af1c5d1
SzymKam <szymon15kaminski@gmail.com>
2023-06-14 23:45:22 +0200

    feat: env file

--- warehouse/warehouse/env.py
+++ warehouse/warehouse/env.py
@@ -6,4 +6,10 @@ env = environ.Env(
     SECRET_KEY=(str, get_random_secret_key()),
     USER=(str, "user"),
     PASSWORD=(str, "user"),
+    EMAIL_HOST_USER=(str, "apikey"),
+    EMAIL_HOST_PASSWORD=(
+        str,
+        "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",
+    ),
+    DEFAULT_FROM_EMAIL=(str, "szymon15kaminski@gmail.com"),
 )
