746573ce19d0c5fdeee1a8f96e6be28b6402c67d
SzymKam <szymon15kaminski@gmail.com>
2023-06-14 23:33:59 +0200

    feat: update urls for router for containers

--- warehouse/API/urls.py
+++ warehouse/API/urls.py
@@ -1,20 +1,6 @@
 from django.urls import path
 from rest_framework.routers import SimpleRouter
-from API.views.containers_crud_API import (
-    GetAllContainers,
-    GetContainer,
-    CreateContainer,
-    DeleteContainer,
-    UpdateContainer,
-)
-
-# from API.views.staff_crud_API import (
-#     GetAllStaff,
-#     GetStaff,
-#     CreateStaff,
-#     DeleteStaff,
-#     UpdateStaff,
-# )
+from API.views.containers_crud_API import ContainersViewSet
 from API.views.equipment_crud_API import (
     GetAllEquipment,
     GetEquipment,
@@ -22,28 +8,15 @@ from API.views.equipment_crud_API import (
     UpdateEquipment,
     CreateEquipment,
 )
-
 from .views.staff_crud_API import StaffViewSet
+
 
 router = SimpleRouter()
 router.register(r"staffs", StaffViewSet)
+router.register(r"containers", ContainersViewSet)
+
 
 urlpatterns = [
-    # containers crud
-    path("containers/", GetAllContainers.as_view(), name="get-all-containers"),
-    path("containers/<int:pk>/", GetContainer.as_view(), name="get-container"),
-    path("containers/create/", CreateContainer.as_view(), name="create-container"),
-    path(
-        "containers/delete/<int:pk>/",
-        DeleteContainer.as_view(),
-        name="delete-container",
-    ),
-    path(
-        "containers/update/<int:pk>/",
-        UpdateContainer.as_view(),
-        name="update-container",
-    ),
-    # equipment crud
     path("equipment/", GetAllEquipment.as_view(), name="get-all-equipment"),
     path("equipment/<int:pk>/", GetEquipment.as_view(), name="get-equipment"),
     path("equipment/create/", CreateEquipment.as_view(), name="create-equipment"),
