c09f40ce7b62fdcb237018d5f0c7a2a60f6cc88e
SzymKam <szymon15kaminski@gmail.com>
2023-06-14 23:33:02 +0200

    feat: update

--- warehouse/API/serializers/staff_serialzer.py
+++ warehouse/API/serializers/staff_serialzer.py
@@ -5,15 +5,4 @@ from staff.models import StaffModel
 class StaffSerializer(serializers.ModelSerializer):
     class Meta:
         model = StaffModel
-        # fields = ("username",
-        #     "first_name",
-        #     "last_name",
-        #     "email",
-        #     "password1",
-        #     "password2",
-        #     "medical_qualifications",
-        #     "position",
-        #     "qualifications_expiration_date",
-        #     "groups",
-        #     "image")
         fields = "__all__"
