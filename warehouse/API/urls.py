f8214d517e4a5bdae4a8ce93d7c050bcd58f19ed
SzymKam <szymon15kaminski@gmail.com>
2023-06-19 23:53:59 +0200

    feat: update urls for new endpoints

--- warehouse/API/urls.py
+++ warehouse/API/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from rest_framework.routers import SimpleRouter
 from API.views.containers_crud_API import ContainersViewSet
 from API.views.equipment_crud_API import (
-    GetAllEquipment,
     GetEquipment,
     DeleteEquipment,
     UpdateEquipment,
     CreateEquipment,
+    EquipmentViewSet,
 )
 from .views.staff_crud_API import StaffViewSet
 
@@ -14,10 +14,11 @@ from .views.staff_crud_API import StaffViewSet
 router = SimpleRouter()
 router.register(r"staffs", StaffViewSet)
 router.register(r"containers", ContainersViewSet)
+router.register(r"equipment", EquipmentViewSet, basename="equipment")
+# router.register(r"drug", )
 
 
 urlpatterns = [
-    path("equipment/", GetAllEquipment.as_view(), name="get-all-equipment"),
     path("equipment/<int:pk>/", GetEquipment.as_view(), name="get-equipment"),
     path("equipment/create/", CreateEquipment.as_view(), name="create-equipment"),
     path(
