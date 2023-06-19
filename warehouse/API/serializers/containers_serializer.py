ae2921475ecd8b81f27ab74b2d05734a31e06856
SzymKam <szymon15kaminski@gmail.com>
2023-06-19 23:52:26 +0200

    feat: update serializer

--- warehouse/API/serializers/containers_serializer.py
+++ warehouse/API/serializers/containers_serializer.py
@@ -1,18 +1,9 @@
 from rest_framework import serializers
 from containers.models import Container
 from .equipment_serializer import AllEquipmentSerializer
-from API.constants import API_NAME_CHOICES
 
 
 class ContainerSerializer(serializers.ModelSerializer):
-    # def create(self, validated_data):
-    #     name = validated_data.get('name')
-    #     allowed_name = API_NAME_CHOICES
-    #     if name in allowed_name:
-    #         return super().create(validated_data)
-    #     else:
-    #         return serializers.ValidationError
-
     class Meta:
         model = Container
         fields = "__all__"
