ae984ed728c8cb9ede4541cab2c315d25665c176
SzymKam <szymon15kaminski@gmail.com>
2023-06-20 17:28:43 +0200

    feat: add new endpoints

--- warehouse/API/urls.py
+++ warehouse/API/urls.py
@@ -1,30 +1,33 @@
-from django.urls import path
 from rest_framework.routers import SimpleRouter
 from API.views.containers_crud_API import ContainersViewSet
 from API.views.equipment_crud_API import (
-    GetEquipment,
-    DeleteEquipment,
-    UpdateEquipment,
-    CreateEquipment,
     EquipmentViewSet,
 )
 from .views.staff_crud_API import StaffViewSet
-
+from API.views.all_models_viewset import (
+    DrugViewset,
+    MedicalEquipmentViewset,
+    FluidViewset,
+    CannulaViewset,
+    NeedleViewset,
+    SyringeViewset,
+    BIGViewset,
+    LtTubeViewset,
+)
 
 router = SimpleRouter()
 router.register(r"staffs", StaffViewSet)
 router.register(r"containers", ContainersViewSet)
 router.register(r"equipment", EquipmentViewSet, basename="equipment")
-# router.register(r"drug", )
+
+router.register(r"drug", DrugViewset)
+router.register(r"medical_equipment", MedicalEquipmentViewset)
+router.register(r"fluid", FluidViewset)
+router.register(r"cannula", CannulaViewset)
+router.register(r"needle", NeedleViewset)
+router.register(r"syringe", SyringeViewset)
+router.register(r"big", BIGViewset)
+router.register(r"lt-tube", LtTubeViewset)
 
 
-urlpatterns = [
-    path("equipment/<int:pk>/", GetEquipment.as_view(), name="get-equipment"),
-    path("equipment/create/", CreateEquipment.as_view(), name="create-equipment"),
-    path(
-        "equipment/delete/<int:pk>/", DeleteEquipment.as_view(), name="delete-equipment"
-    ),
-    path(
-        "equipment/update/<int:pk>/", UpdateEquipment.as_view(), name="update-equipment"
-    ),
-] + router.urls
+urlpatterns = [] + router.urls
