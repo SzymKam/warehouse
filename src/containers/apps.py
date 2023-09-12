26209f9697ccbdb2b7efa2b9ca663826cfacb3de
SzymKam <szymon15kaminski@gmail.com>
2023-09-12 23:25:03 +0200

    fix: add ready function which improts singals file

--- src/containers/apps.py
+++ src/containers/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class ContainersConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "containers"
+
+    def ready(self):
+        import containers.signals
