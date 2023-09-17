6c33491005e4f210f33a3b47238ccaea7259a852
SzymKam <szymon15kaminski@gmail.com>
2023-09-17 09:30:54 +0200

    fix: change variables into default values

--- src/core/env.py
+++ src/core/env.py
@@ -2,11 +2,11 @@ import environ
 from django.core.management.utils import get_random_secret_key
 
 env = environ.Env(
-    DEBUG=(bool, True),
+    DEBUG=(bool, False),
     SECRET_KEY=(str, get_random_secret_key()),
-    USER=(str, "warehouse_user"),
-    PASSWORD=(str, "warehouse_user"),
-    HOST=(str, "db"),
+    USER=(str, ""),
+    PASSWORD=(str, ""),
+    HOST=(str, ""),
     EMAIL_HOST_USER=(str, "apikey"),
     EMAIL_HOST_PASSWORD=(
         str,
