89dbc48ec7b5c4b7d382198aba21f2aa2ca8bcf9
SzymKam <szymon15kaminski@gmail.com>
2023-10-25 15:36:44 +0200

    fix: change collect static urls into main url file

--- src/core/urls.py
+++ src/core/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+from .env import env
 
 
 urlpatterns = [
@@ -25,3 +28,8 @@ urlpatterns = [
 ]
 
 handler403 = "containers.views.error_403.error_403"
+
+
+if env("DEBUG"):
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
