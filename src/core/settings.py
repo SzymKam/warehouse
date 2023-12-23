632c23c9790717e9a94f91ceca339c6b6f709eca
SzymKam <szymon15kaminski@gmail.com>
2023-12-23 08:28:07 +0100

    fix: update env file read

--- src/core/settings.py
+++ src/core/settings.py
@@ -17,7 +17,8 @@ from .env import env
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-env.read_env(".env")
+
+env.read_env(os.path.join(BASE_DIR.parent, ".env"))
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
