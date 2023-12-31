eefade63e97f88d13268e323d4569a5dc8368765
SzymKam <szymon15kaminski@gmail.com>
2023-12-31 08:02:42 +0100

    fix: update settings for aws s3

--- src/core/settings.py
+++ src/core/settings.py
@@ -195,12 +195,15 @@ AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = env("AWS_STORAGE_BUCKET_NAME")
 AWS_REGION_NAME = env("AWS_REGION_NAME")
 AWS_S3_REGION_NAME = env("AWS_REGION_NAME")
+AWS_DEFAULT_ACL = "public-read"
 
 AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 AWS_S3_OBJECT_PARAMETERS = {
     "CacheControl": "max-age=86400",
 }
 AWS_LOCATION = "static"
+
+DEFAULT_FILE_STORAGE = "storages.backends.s3.S3Storage"
 STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
