ecb0d85797f9820641dc7f4684c2a57beeee1a5d
SzymKam <szymon15kaminski@gmail.com>
2023-12-31 15:01:36 +0100

    fix: change few of aws s3 settings variables

--- src/core/settings.py
+++ src/core/settings.py
@@ -201,23 +201,22 @@ EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = env("DEFAULT_FROM_EMAIL")
 
 """AWS settings for s3"""
-AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY")
+# *AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID")
+# *AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY")
 
-AWS_STORAGE_BUCKET_NAME = env("AWS_STORAGE_BUCKET_NAME")
-AWS_REGION_NAME = env("AWS_REGION_NAME")
-AWS_S3_REGION_NAME = env("AWS_REGION_NAME")
-AWS_DEFAULT_ACL = "public-read"
+AWS_STORAGE_BUCKET_NAME = "warehouse-s3-bucket"
+AWS_REGION_NAME = "eu-central-1"
+AWS_S3_REGION_NAME = "eu-central-1"
 
 AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 AWS_S3_OBJECT_PARAMETERS = {
     "CacheControl": "max-age=86400",
 }
-AWS_LOCATION = "static"
-
-DEFAULT_FILE_STORAGE = "storages.backends.s3.S3Storage"
 STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
-STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+
+# AWS_LOCATION = "static"
+# DEFAULT_FILE_STORAGE = "storages.backends.s3.S3Storage"
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
+# ]
+# STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
