612c54f888533f9294780c2a4a4f8b0bb6d7b694
SzymKam <szymon15kaminski@gmail.com>
2024-01-01 21:07:13 +0100

    fix: working on aws s3 settings

--- src/core/settings.py
+++ src/core/settings.py
@@ -212,11 +212,11 @@ AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 AWS_S3_OBJECT_PARAMETERS = {
     "CacheControl": "max-age=86400",
 }
-STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+# STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
-# AWS_LOCATION = "static"
+AWS_LOCATION = "static"
 # DEFAULT_FILE_STORAGE = "storages.backends.s3.S3Storage"
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, "static"),
 # ]
-# STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
